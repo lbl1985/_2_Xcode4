39bca04572346db17ff591a077288def2ec4749c
Binlong Li <herbert19lee@gmail.com>
2012-01-19 21:40:50 -0500

    Initial Commit

---
+++ _2_Xcode4/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  _2_Xcode4
+//
+//  Created by Binlong Li on 1/19/12.
+//  Copyright 2012 Northeaster University. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
